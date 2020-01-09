fa7a2f8fa88947700d6877abb9efc3c3be91a133
AndreeVelasco <50391794+jtamayo26@users.noreply.github.com>
2020-01-09 18:23:31 -0500

    implementando permisos

--- app/src/main/java/com/example/u1tema2/MainActivity.java
+++ app/src/main/java/com/example/u1tema2/MainActivity.java
@@ -40,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, Multimedia.class));
             return true;
         }
+        if (id == R.id.activity_permisos) {
+            startActivity(new Intent(this, Permisos.class));
+            return true;
+        }
 
         if (id == R.id.activity_intenciones) {
             startActivity(new Intent(this, intenciones.class));
