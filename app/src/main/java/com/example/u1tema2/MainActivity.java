40d7079da237979649c1534c1acbbf6cbb72cacd
LeydiHuallpa <leydihuallpa67@gmail.com>
2020-01-21 09:56:01 -0500

    incluye navigationdrawer

--- app/src/main/java/com/example/u1tema2/MainActivity.java
+++ app/src/main/java/com/example/u1tema2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.u1tema2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,14 +15,28 @@ import android.view.MenuItem;
 import com.example.u1tema2.FragmentTabHost.MiFragmentTabhost;
 import com.example.u1tema2.Multimedia.Multimedia;
 import com.example.u1tema2.mirecyclerview.MiRecyclerView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,4 +83,29 @@ public class MainActivity extends AppCompatActivity {
             return super.onOptionsItemSelected(item);
         }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_compartir:
+                Intent paramView;
+                paramView = new Intent("android.intent.action.SEND");
+                paramView.setType("text/plain");
+                paramView.putExtra("android.intent.extra.TEXT", "Descarga nuestra app de la PlayStore" +
+                        " \n" + "https://play.google.com/store/apps/details?id=app.product.demarktec.alo14_pasajero");
+                startActivity(Intent.createChooser(paramView, "Comparte Nuestro aplicativo"));
+                break;
+            case R.id.nav_salir:
+                finish();
+                break;
+            default:
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
+
